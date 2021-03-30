0fd45de8fb5b141bd5dba22822419cd08e1a9b20
J-Phiz <jp.save@gmail.com>
2021-03-30 15:47:23 +0200

    Ajout des listeners pour la gestion des modifications

--- src/fr/jpsave/colorpicker/controller/ColorPickerController.java
+++ src/fr/jpsave/colorpicker/controller/ColorPickerController.java
@@ -1,12 +1,10 @@
 package fr.jpsave.colorpicker.controller;
 
 import fr.jpsave.colorpicker.model.Color;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -61,7 +59,77 @@ public class ColorPickerController implements Initializable {
 
         // Initialize Model with black Color
         color = new Color("#000000");
-    }
 
+        // Add listener on each slider
+        sliderRed.valueProperty().addListener((ov, old_val, new_val) -> {
+            color.setRed((int) sliderRed.valueProperty().get());
+            textFieldRed.setText(Integer.toString(color.getRed()));
+            textFieldHex.setText(color.getRgbCode());
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+        sliderGreen.valueProperty().addListener((ov, old_val, new_val) -> {
+            color.setGreen((int) sliderGreen.valueProperty().get());
+            textFieldGreen.setText(Integer.toString(color.getGreen()));
+            textFieldHex.setText(color.getRgbCode());
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+        sliderBlue.valueProperty().addListener((ov, old_val, new_val) -> {
+            color.setBlue((int) sliderBlue.valueProperty().get());
+            textFieldBlue.setText(Integer.toString(color.getBlue()));
+            textFieldHex.setText(color.getRgbCode());
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+
+        // Add listener on each textField
+        textFieldRed.textProperty().addListener((ov, old_val, new_val) -> {
+            try {
+                color.setRed(Integer.parseInt(textFieldRed.textProperty().get()));
+            }
+            catch (IllegalArgumentException e) {
+                color.setRed(0);
+            }
+            sliderRed.setValue(color.getRed());
+            textFieldHex.setText(color.getRgbCode());
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+        textFieldGreen.textProperty().addListener((ov, old_val, new_val) -> {
+            try {
+                color.setGreen(Integer.parseInt(textFieldGreen.textProperty().get()));
+            }
+            catch (IllegalArgumentException e) {
+                color.setGreen(0);
+            }
+            sliderGreen.setValue(color.getGreen());
+            textFieldHex.setText(color.getRgbCode());
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+        textFieldBlue.textProperty().addListener((ov, old_val, new_val) -> {
+            try {
+                color.setBlue(Integer.parseInt(textFieldBlue.textProperty().get()));
+            }
+            catch (IllegalArgumentException e) {
+                color.setBlue(0);
+            }
+            sliderBlue.setValue(color.getBlue());
+            textFieldHex.setText(color.getRgbCode());
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+
+        textFieldHex.textProperty().addListener((ov, old_val, new_val) -> {
+            try {
+                color.setRgbCode(textFieldHex.textProperty().get());
+            }
+            catch (IllegalArgumentException e) {
+                color.setRgbCode("#");
+            }
+            sliderRed.setValue(color.getRed());
+            sliderGreen.setValue(color.getGreen());
+            sliderBlue.setValue(color.getBlue());
+            textFieldRed.setText(Integer.toString(color.getRed()));
+            textFieldGreen.setText(Integer.toString(color.getGreen()));
+            textFieldBlue.setText(Integer.toString(color.getBlue()));
+            paneColor.setStyle("-fx-background-color: " + color.getRgbCode());
+        });
+    }
 
 }
